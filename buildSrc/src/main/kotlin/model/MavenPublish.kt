c8615daedf53b7ccd4e6c8e582fc01f8a241241b
szhangbiao <szhangbiao@gmail.com>
2025-01-14 17:31:55 +0800

    Update maven config

--- buildSrc/src/main/kotlin/model/MavenPublish.kt
+++ buildSrc/src/main/kotlin/model/MavenPublish.kt
@@ -4,5 +4,5 @@ data class MavenPublish(
     val artifactId: String,
     val version: String, //X.Y.Z; X = Major, Y = minor, Z = Patch (endsWith('-SNAPSHOT') ? "Debug" : "Release")
     val productFlavor:String = "release",
-    val groupId: String = "com.liulishuo.okdownload"
+    val groupId: String = "com.yunqinglai.okdownload"
 )
